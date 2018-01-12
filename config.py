757c65a78f55d3deededf1d650de79892f7bbd09
Robert West <rwest@remedypartners.com>
2018-01-12 14:49:23 +0000

    updated functionality to include file count in config params

--- config.py
+++ config.py
@@ -3,8 +3,14 @@
 default_params_sets = {
 
     'lds-transform-reruns': { 
+        'job': 'preview',
         'pre': 's3://rmdy-analytics-lds-out/logs/11.20.2017/transform/',
         'after-time': '/reruns_needed/'
+    },
+    'lds-transform-completed': { 
+        'job': 'count',
+        'pre': 's3://rmdy-analytics-lds-out/logs/11.20.2017/transform/',
+        'after-time': '/completed/'
     }
 }
     
