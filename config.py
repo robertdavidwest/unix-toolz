488f091666bec249aef0d466a385bb54e1e7a217
Robert West <rwest@remedypartners.com>
2018-01-12 15:19:09 +0000

    generic config example

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 
 default_params_sets = {
 
-    'lds-transform-reruns': { 
+    'lds-{process}-reruns': { 
         'job': 'preview',
-        'pre': 's3://rmdy-analytics-lds-out/logs/11.20.2017/transform/',
+        'pre': 's3://{bucket}/logs/11.20.2017/{process}/',
         'after-time': '/reruns_needed/'
     },
-    'lds-transform-completed': { 
+    'lds-{process}-completed': { 
         'job': 'count',
-        'pre': 's3://rmdy-analytics-lds-out/logs/11.20.2017/transform/',
+        'pre': 's3://{bucket}/logs/11.20.2017/{process}/',
         'after-time': '/completed/'
     }
 }
