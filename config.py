984916c916c072ccf0d2ac0f36fd7845604b6a17
Robert West <rwest@remedypartners.com>
2018-01-13 14:01:52 +0000

    err handling, print total files in dir when previewing

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ default_params_sets = {
     'lds-transform-reruns': { 
         'job': 'preview',
         'pre': 's3://rmdy-analytics-lds-out/logs/11.20.2017/transform/',
-        'run-time': '2018-01-10_15.26.14',  # 'use-latest'
+        'run-time': 'use-latest', #'2018-01-10_15.26.14',  # 'use-latest'
         'after-time': '/reruns_needed/'
     },
     'lds-transform-completed': { 
         'job': 'count',
         'pre': 's3://rmdy-analytics-lds-out/logs/11.20.2017/transform/',
-        'run-time':  '2018-01-10_15.26.14', # 'use-latest'
+        'run-time':  'use-latest', #'2018-01-10_15.26.14', # 'use-latest'
         'after-time': '/completed/'
     },
 }
